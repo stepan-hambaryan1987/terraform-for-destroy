fafbdfc700277295ede3a07389b1d48e15ba0976
stepan hambaryan <stepan-hambaryan1987@mail.ru>
2025-09-08 12:34:32 +0400

    added new resourses in main.tf

--- main.tf
+++ main.tf
@@ -32,32 +32,75 @@ resource "google_compute_firewall" "my_firewall" {
 #   name   = "new-external-ip"
 #   region = "asia-northeast3"
 # }
-
-
 variable "zones" {
   default = ["asia-northeast3-a", "asia-northeast3-b", "asia-northeast3-c"]
 }
-# 4. Create VM Instance
-resource "google_compute_instance" "vm_instance" {
-  count        = 1
-  name         = format("my-new-instance-%02d", count.index + 1) # my-instance-01 ... my-instance-10
-  machine_type = "e2-micro"
-  zone         = var.zones[count.index % length(var.zones)]
 
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
+# 4. Create VM Instance
+# resource "google_compute_instance" "vm_instance" {
+#   count        = 1
+#   name         = format("my-new-instance-%02d", count.index + 1) # my-instance-01 ... my-instance-10
+#   machine_type = "e2-micro"
+#   zone         = var.zones[count.index % length(var.zones)]
+
+#   boot_disk {
+#     initialize_params {
+#       image = "debian-cloud/debian-11"
+#     }
+#   }
+
+#   network_interface {
+#     network    = google_compute_network.vpc_network.id
+#     subnetwork = google_compute_subnetwork.pub_subnet.id
+
+#     access_config {}
+#   }
+
+#   tags = ["ssh"]
+#   metadata = {
+#   startup-script = <<-EOT
+#     #!/bin/bash
+#     apt-get update
+#     apt-get install -y nginx
+#     systemctl enable nginx
+#     systemctl start nginx
+
+#     cat <<EOF > /var/www/html/index.html
+#     <html>
+#       <head>
+#         <title>Welcome</title>
+#       </head>
+#       <body style="background-color: black; color: yellow; text-align: center; font-size: 24px; margin-top: 20%;">
+#         barev im sireli Arevikin, es qez shat em sirum, du im hrashqn es $(hostname)
+#       </body>
+#     </html>
+#     EOF
+#   EOT
+# }
+
+
+# ============================
+# 1. Instance Template
+# ============================
+resource "google_compute_instance_template" "vm_template" {
+  name         = "my-instance-template"
+  machine_type = "e2-micro"
+
+  tags = ["ssh", "http"]
+
+  disk {
+    auto_delete  = true
+    boot         = true
+    source_image = "debian-cloud/debian-11"
   }
 
   network_interface {
     network    = google_compute_network.vpc_network.id
     subnetwork = google_compute_subnetwork.pub_subnet.id
 
-    access_config {}
+    access_config {} # External IP
   }
 
-  tags = ["ssh"]
   metadata = {
   startup-script = <<-EOT
     #!/bin/bash
@@ -77,8 +120,107 @@ resource "google_compute_instance" "vm_instance" {
     </html>
     EOF
   EOT
+ }
 }
 
+# ============================
+# 2. Managed Instance Group
+# ============================
+resource "google_compute_instance_group_manager" "vm_group" {
+  name               = "my-instance-group"
+  base_instance_name = "my-new-instance"
+  zone               = var.zones[0]
+  target_size        = 2
+
+  auto_healing_policies {
+    health_check      = google_compute_health_check.default.id
+    initial_delay_sec = 300
+  }
+
+  version {
+    instance_template = google_compute_instance_template.vm_template.id
+  }
+
+  named_port {
+    name = "http"
+    port = 80
+  }
 }
 
+
+# ============================
+# 3. Autoscaler
+# ============================
+
+resource "google_compute_autoscaler" "vm_group_autoscaler" {
+  name   = "my-instance-autoscaler"
+  zone   = var.zones[0]
+  target = google_compute_instance_group_manager.vm_group.id
+
+  autoscaling_policy {
+    min_replicas = 2
+    max_replicas = 5
+    cpu_utilization {
+      target = 0.6 # scale out when average CPU > 60%
+    }
+  }
+}
+
+# ============================
+# 4. Health Check (for LB)
+# ============================
+resource "google_compute_health_check" "default" {
+  name                = "http-health-check"
+  check_interval_sec  = 5
+  timeout_sec         = 5
+  healthy_threshold   = 2
+  unhealthy_threshold = 2
+
+  http_health_check {
+    port = 80
+  }
+}
+
+# ============================
+# 5. Backend Service
+# ============================
+resource "google_compute_backend_service" "default" {
+  name                            = "backend-service"
+  protocol                        = "HTTP"
+  port_name                       = "http"
+  timeout_sec                     = 10
+  health_checks                   = [google_compute_health_check.default.id]
+  connection_draining_timeout_sec = 0
+
+  backend {
+    group = google_compute_instance_group_manager.vm_group.instance_group
+  }
+
+  depends_on = [google_compute_health_check.default]
+}
+
+# ============================
+# 6. URL Map LB
+# ============================
+resource "google_compute_url_map" "default" {
+  name            = "url-map"
+  default_service = google_compute_backend_service.default.id
+}
+
+# ============================
+# 7. HTTP Proxy
+# ============================
+resource "google_compute_target_http_proxy" "default" {
+  name    = "http-proxy"
+  url_map = google_compute_url_map.default.id
+}
+
+# ============================
+# 8. Global Forwarding Rule
+# ============================
+resource "google_compute_global_forwarding_rule" "default" {
+  name       = "http-forwarding-rule"
+  port_range = "80"
+  target     = google_compute_target_http_proxy.default.id
+}
 
